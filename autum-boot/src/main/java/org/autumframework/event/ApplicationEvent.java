58b40b8e3412cf785323e9c679e98f4e9fb209b9
Hadush <had.hailu@gmail.com>
2024-08-13 08:34:12 -0500

    @Async: enable threading if @Async annotation is present

--- autum-boot/src/main/java/org/autumframework/event/ApplicationEvent.java
+++ autum-boot/src/main/java/org/autumframework/event/ApplicationEvent.java
@@ -1,6 +1,8 @@
 package org.autumframework.event;
 
 import org.autumframework.annotation.Service;
+import org.autumframework.customClass.CustomMethod;
+import org.autumframework.threading.AsyncExecutor;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -10,25 +12,38 @@ import java.util.Map;
 @Service
 public class ApplicationEvent {
 
-    private static Map<Object, Map<Object, Method>> events = new HashMap<>();
+    private static Map<Object, Map<Object, CustomMethod>> events = new HashMap<>();
 
-    public static void setEvents(Map<Object, Map<Object, Method>> events) {
+
+    public static void setEvents(Map<Object, Map<Object, CustomMethod>> events) {
         ApplicationEvent.events = events;
     }
 
     public void publishEvent(Object object){
 
-        Map<Object,Method> methods = events.get(object.getClass().getName());
+        Map<Object,CustomMethod> methods = events.get(object.getClass().getName());
         if(methods != null) {
             methods.forEach((k,v)->{
-                v.setAccessible(true);
-                try {
-                    v.invoke(k,object);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    throw new RuntimeException(e);
+                System.out.println("[=============++++] "+k.getClass()+" isAsync="+v.isAsync());
+                if(v.isAsync()){
+                    System.out.println("[========+++++] Application event started");
+                    new Thread(new AsyncExecutor(v.getMethod(), k, object)).start();
+                    System.out.println("[========+++++] Application event finished");
+                }else{
+                    this.serialExecutor(v.getMethod(), k, object);
                 }
+
             });
         }
 
+    }
+
+    public void serialExecutor(Method method, Object object, Object param){
+        method.setAccessible(true);
+        try {
+            method.invoke(object,param);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
